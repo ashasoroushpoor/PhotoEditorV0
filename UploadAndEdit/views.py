825899952a69874508d9bb99b52da66fa3bd9123
AshaSoroushpoor <ashasoroushpoor@gmail.com>
2019-02-13 03:43:10 +0330

    all finished....just need some style

--- UploadAndEdit/views.py
+++ UploadAndEdit/views.py
@@ -30,8 +30,18 @@ def edit(request):
 
 
 def crop(request):
-
-    return HttpResponse()
+    photo = Photo.objects.get(pk=request.session['photo_pk'])
+    pil_image = Image.open(photo.file.url[1:])
+    changed_image = pil_image.crop((int(request.POST.get('left')),
+                                    int(request.POST.get('upper')),
+                                    int(request.POST.get('right')),
+                                    int(request.POST.get('lower'))))
+    changed_image.save(photo.photo.url[1:])
+    photo.save(update_fields=['file'])
+    context = {
+        'photo': photo,
+    }
+    return render(request, 'photo/edit.html', context)
 
 
 def bw(request):
@@ -48,14 +58,37 @@ def bw(request):
 
 
 def rotate(request):
-
-    return HttpResponse()
+    photo = Photo.objects.get(pk=request.session['photo_pk'])
+    pil_image = Image.open(photo.file.url[1:])
+    changed_image = pil_image.rotate(int(request.POST.get('angle')),
+                                     expand=1)
+    changed_image.save(photo.file.url[1:])
+    photo.save(update_fields=['file'])
+    context = {
+        'photo': photo,
+    }
+    return render(request, 'photo/edit.html', context)
 
 
 def resize(request):
+    photo = Photo.objects.get(pk=request.session['photo_pk'])
+    pil_image = Image.open(photo.file.url[1:])
+    changed_image = pil_image.resize((int(request.POST.get('width')),
+                                      int(request.POST.get('height'))))
+    changed_image.save(photo.file.url[1:])
+    photo.save(update_fields=['file'])
+    context = {
+        'photo': photo,
+    }
+    return render(request, 'photo/edit.html', context)
 
-    return HttpResponse()
 
 def share(request):
-
-    return HttpResponse()
+    photo = Photo.objects.get(pk=request.session['photo_pk'])
+    pil_image = Image.open(photo.file.url[1:])
+    photo.shared = True
+    photo.save(update_fields=['shared'])
+    context = {
+        'photo': photo,
+    }
+    return render(request, 'photo/edit.html', context)
